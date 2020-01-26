0a70eacd7611f3b06dac3ec9eb7b22022600ca13
tawfiqsiam <toto.siam999@gmail.com>
2020-01-26 02:59:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv-flow').config();
 
 module.exports = {
-    owner: 263639609416613888 ,
-    prefix: . ,
+    owner: 263639609416613888
+    prefix:.
     mongoURI: cluster0-shard-00-00-89nkx.mongodb.net:27017,
     defaultSettings: {
         prefix: process.env.PREFIX,
